9b836ad685146a79368f42246ac9c34f2f1e666b
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-10-22 22:26:20 +0300

    Refactor parameters

--- 1homework.py
+++ 1homework.py
@@ -19,54 +19,54 @@ class firsttest(unittest.TestCase):
     def test_firsttest(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_group_page(wd)
         self.create_new_group(wd)
-        self.fill_data_new_group(wd)
+        self.fill_data_new_group(wd, name="ftft", header="tftf", footer="tegvetr")
         self.new_group_submit(wd)
         self.open_group_page_again(wd)
         self.logout(wd)
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
-    def open_group_page_again(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def new_group_submit(self, wd):
-        wd.find_element_by_name("submit").click()
-
-    def fill_data_new_group(self, wd):
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("ftft")
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("tftf")
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("tegvetr")
-
-    def create_new_group(self, wd):
-        wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
-
-    def open_group_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd):
+    def login(self, wd, username, password):
         # login
         wd.find_element_by_css_selector("html").click()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def create_new_group(self, wd):
+        wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
+
+    def fill_data_new_group(self, wd, name, header, footer):
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(footer)
+
+    def new_group_submit(self, wd):
+        wd.find_element_by_name("submit").click()
+
+    def open_group_page_again(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
         self.wd.quit()
