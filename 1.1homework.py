43b2ae12d5eb12bd380d8276eaf1c91682437222
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-10-22 23:28:45 +0300

    Add additional class Contact

--- 1.1homework.py
+++ 1.1homework.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
+from contact import Contact
 import unittest
 
 
@@ -21,9 +22,10 @@ class secondtest(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_new_contact(wd)
-        self.fill_new_contact(wd, firstname="Ivan", middlename="\\9", lastname="Ivanov", nickname="ivan299",
-                         title="Test contact", companyname="Test company", companyaddress="123", homephone="+7000000000",
-                         firstmail="test@mail.ru", homepage="http://localhost/addressbook/edit.php")
+        self.fill_new_contact(wd, Contact(firstname="Ivan", middlename="\\9", lastname="Ivanov", nickname="ivan299",
+                              title="Test contact", companyname="Test company", companyaddress="123",
+                              homephone="+7000000000", firstmail="test@mail.ru",
+                              homepage="http://localhost/addressbook/edit.php"))
         self.submit_new_contact(wd)
         self.open_home(wd)
         self.logout(wd)
@@ -37,39 +39,37 @@ class secondtest(unittest.TestCase):
     def submit_new_contact(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def fill_new_contact(self, wd, firstname="Ivan", middlename="\\9", lastname="Ivanov", nickname="ivan299",
-                         title="Test contact", companyname="Test company", companyaddress="123", homephone="+7000000000",
-                         firstmail="test@mail.ru", homepage="http://localhost/addressbook/edit.php"):
+    def fill_new_contact(self, wd, contact):
         # fill data for new contact
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(contact.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(companyname)
+        wd.find_element_by_name("company").send_keys(contact.companyname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(companyaddress)
+        wd.find_element_by_name("address").send_keys(contact.companyaddress)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(homephone)
+        wd.find_element_by_name("home").send_keys(contact.homephone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(firstmail)
+        wd.find_element_by_name("email").send_keys(contact.firstmail)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(homepage)
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
 
     def open_new_contact(self, wd):
         wd.find_element_by_link_text("add new").click()
---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, companyname, companyaddress,
+                         homephone, firstmail, homepage):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.companyname = companyname
+        self.companyaddress = companyaddress
+        self.homephone = homephone
+        self.firstmail = firstmail
+        self.homepage = homepage
