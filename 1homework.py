1bbd92f679b3b07640ce01c51d0f7e23116f18bc
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-10-24 20:42:49 +0300

    Remove WD

--- 1homework.py
+++ 1homework.py
@@ -18,24 +18,24 @@ class firsttest(unittest.TestCase):
         self.wd.implicitly_wait(60)
 
     def test_firsttest(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.fill_data_new_group(wd, Group(name="ftft", header="tftf", footer="tegvetr"))
-        self.logout(wd)
+
+        self.login(username="admin", password="secret")
+        self.fill_data_new_group(Group(name="ftft", header="tftf", footer="tegvetr"))
+        self.logout()
 
     def test_firsttest_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.fill_data_new_group(wd, Group(name="", header="", footer=""))
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.fill_data_new_group(Group(name="", header="", footer=""))
+        self.logout()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
         # login
-        self.open_home_page(wd)
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_css_selector("html").click()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -45,16 +45,19 @@ class firsttest(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def open_group_page(self, wd):
+    def open_group_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create_new_group(self, wd):
-        self.open_group_page(wd)
+    def create_new_group(self):
+        wd = self.wd
+        self.open_group_page()
         wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
 
-    def fill_data_new_group(self, wd, group):
+    def fill_data_new_group(self, group):
         # fill group form
-        self.create_new_group(wd)
+        wd = self.wd
+        self.create_new_group()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -64,15 +67,18 @@ class firsttest(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        self.open_group_page_again(wd)
+        self.open_group_page_again()
 
-    def new_group_submit(self, wd):
+    def new_group_submit(self):
+        wd = self.wd
         wd.find_element_by_name("submit").click()
 
-    def open_group_page_again(self, wd):
+    def open_group_page_again(self):
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
