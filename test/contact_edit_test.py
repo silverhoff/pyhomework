bb4147d1180005ea0d0a198af3c62c1bbbc73562
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-10-30 22:16:57 +0300

    minor fixes
    remove some login and logout from middle tests

--- test/contact_edit_test.py
+++ test/contact_edit_test.py
@@ -2,7 +2,7 @@ from model.contact import Contact
 
 def test_contact_edit(app):
     app.open_home_page()
-    app.session.login(username="admin", password="secret")
+    #app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname="Boris", middlename="Borisovich", lastname="Borisov", nickname="Boris122",
                                   title="Second contact", companyname="Second company", companyaddress="321",
                                   homephone="+71111111111", firstmail="test123@mail.ru",
