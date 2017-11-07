41ac2db43ae09f253b004323809e5957fa135254
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-11-07 12:45:48 +0300

    add group count check

--- test/add_group_test.py
+++ test/add_group_test.py
@@ -3,7 +3,13 @@ from model.group import Group
 
 
 def test_firsttest(app):
+    old_groups = app.group.get_group_list()
     app.group.create_new_group(Group(name="ftft", header="tftf", footer="tegvetr"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_firsttest_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create_new_group(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/group_edit_test.py
+++ test/group_edit_test.py
@@ -4,9 +4,15 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create_new_group(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify(Group(name="abc"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create_new_group(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.modify(Group(header="cba"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
