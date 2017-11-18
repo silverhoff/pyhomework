1ed5750b4f4a1aa73c47aeb1f4aa605804b6e5ca
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-11-18 09:51:41 +0300

    add sort for del tests

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -12,7 +12,7 @@ def test_delete_first_contact(app):
     app.open_home_page()
     app.contact.delete_by_index(index)
     app.open_home_page()
+    assert len(old_contacts) - 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[index:index+1] = []
-    assert old_contacts == new_contacts
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
