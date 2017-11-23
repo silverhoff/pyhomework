423f061d3480cbd694181361c7de20bbd79eb176
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-11-23 22:01:54 +0300

    parametrize add tests

--- test/add_contact_test.py
+++ test/add_contact_test.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_secondtest(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10), lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+                                  title=random_string("title", 10), companyname=random_string("companyname", 10), companyaddress=random_string("companyaddress", 10),
+                                  homephone=random_string("homephone", 11), mobilephone=random_string("mobilephone", 11), workphone=random_string("workphone", 11), secondaryphone=random_string("secondaryphone", 10), firstmail=random_string("firstmail", 10),
+                                  secondmail=random_string("secondmail", 10), thirdmail=random_string("thirdmail", 10),
+                                  homepage=random_string("homepage", 10))
+                                                     for i in range(3)
+            ]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_secondtest(app, contact):
     old_contacts = app.contact.get_contact_list()
     app.contact.open_new()
-    contact = Contact(firstname="Ivan", middlename="\\9", lastname="Ivanov", nickname="ivan299",
-                                  title="Test contact", companyname="Test company", companyaddress="123",
-                                  homephone="700000000", mobilephone="52353452345", workphone="10000002020", secondaryphone="42134123412", firstmail="test@mail.ru",
-                                  secondmail="test2@mail.ru", thirdmail="test3@mail.ru",
-                                  homepage="http://localhost/addressbook/edit.php")
     app.contact.fill_data(contact)
     app.contact.submit_new()
     app.contact.open_home()
