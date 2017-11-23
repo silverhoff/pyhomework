846188922f52f9eb9f01b0268f2e437dbc7fee01
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-11-23 22:29:03 +0300

    add cmd options

--- conftest.py
+++ conftest.py
@@ -6,11 +6,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseurl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -21,3 +23,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
