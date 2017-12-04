86e3e21c974d6289ae1c10e7c4546759fd829e8a
silverhoff <32219213+silverhoff@users.noreply.github.com>
2017-12-04 23:14:10 +0300

    add db load to contact edit test

--- test/contact_edit_test.py
+++ test/contact_edit_test.py
@@ -1,7 +1,7 @@
 from model.contact import Contact
 from random import randrange
 
-def test_contact_edit_first_part(app):
+def test_contact_edit_first_part(app, db , check_ui):
     if app.contact.count() == 0:
         app.open_home_page()
         app.contact.open_new()
@@ -9,16 +9,16 @@ def test_contact_edit_first_part(app):
         app.contact.submit_new()
     contact = Contact(firstname="Boris", middlename="Borisovich", lastname="Borisov", nickname="Boris122",
                                title="Second contact")
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     contact.id = old_contacts[index].id
 #    app.contact.open_new()
     app.open_home_page()
     app.contact.modify_by_index(index, contact)
     app.open_home_page()
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    new_contacts = db.get_contact_list()
+    if check_ui:
+        assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
     #def test_contact_edit_second_part(app):
